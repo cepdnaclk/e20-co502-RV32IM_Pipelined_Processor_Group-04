ba3a99ad33ff613c37b55520baaf02d4ea534604
Chethiya Bandara <e20032@eng.pdn.ac.lk>
2025-04-12 14:02:03 +0530

    assembler

--- Assembler/testcases/test.s
+++ Assembler/testcases/test.s
@@ -1,3 +1,14 @@
-addi x1, x1, 15
-addi x2, x2, -5
-add x3, x1, x2
+addi x5, x0, 10
+addi x1, x5, -50
+sub x5, x1, x3
+sub x5, x0, x3
+mul x5, x1, x3
+div x5, x3, x7
+rem x5, x1, x7
+sw x1, 0(x0)
+sub x5, x0, x3
+NOP
+NOP
+lw x7, 0(x0)
+add x16, x7, x0
+beq x0, x0, -2
